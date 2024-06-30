049108f3b2b5f21401fc495e49b4d7de020f8194
wborba195 <willianborbadasilva@yahoo.com.br>
2024-06-30 18:57:26 -0300

    Ajustes funçaõ Rendimento

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Calendar;
 
 public class Main {
 
@@ -8,15 +7,18 @@ public class Main {
 		
 		Conta cc = new ContaCorrente(venilton);
 		ContaPoupanca poupanca = new ContaPoupanca(venilton);
-		Calendar cal = Calendar.getInstance();
-		poupanca.dataRendimento = cal.get(Calendar.DAY_OF_MONTH)+"/"+cal.get(Calendar.MONTH)+"/"+cal.get(Calendar.YEAR);
-		System.out.println("Data Aplicação: " +poupanca.dataAplica);
-		System.out.println("Data Rendimento: " +poupanca.dataRendimento);
-		System.out.println("Data Hoje: " +poupanca.dataHoje);
+		//Calendar cal = Calendar.getInstance();
+		//poupanca.dataRendimento = cal.get(Calendar.DAY_OF_MONTH)+"/"+cal.get(Calendar.MONTH)+"/"+cal.get(Calendar.YEAR);
+
+		//System.out.println("Data Hoje: " +poupanca.dataHoje);
 		
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
+		
+		System.out.println("Data Aplicação: " +poupanca.dataAplica);
+		System.out.println("Data Rendimento: " +poupanca.dataRendimento);
 		poupanca.rendimento();
+		
 		cc.imprimirExtrato();		
 		poupanca.imprimirExtrato();
 	}
